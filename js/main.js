99c4dd3c5026a8cf3228ffd4e0db04052fe07ea4
qts <2100012999@stu.pku.edu.cn>
2024-05-18 19:17:46 +0800

    home WIP

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    centeredSlides: true,
+    autoplay: 2500,
+    autoplayDisableOnInteraction: false
+});
