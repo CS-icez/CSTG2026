6e34b09e3c01924836594e4b9c1798f3ba274a91
qts <2100012999@stu.pku.edu.cn>
2024-05-19 10:59:38 +0800

    nits

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ const swiper = new Swiper('.swiper-container', {
 
 $(function () {
     initShowAll();
+    initModal();
 })
 
 function initShowAll() {
@@ -18,6 +19,46 @@ function initShowAll() {
             $(this).html('<strong>Hide List</strong>');
         } else {
             $(this).html('<strong>Show All</strong>');
+        }
+    });
+}
+
+function initModal() {
+    console.log('initModal');
+    function openModal($el) {
+        $el.classList.add('is-active');
+    }
+
+    function closeModal($el) {
+        $el.classList.remove('is-active');
+    }
+
+    function closeAllModals() {
+        (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+            closeModal($modal);
+        });
+    }
+
+    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+        const modal = $trigger.dataset.target;
+        const $target = document.getElementById(modal);
+
+        $trigger.addEventListener('click', () => {
+            openModal($target);
+        });
+    });
+
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+        const $target = $close.closest('.modal');
+
+        $close.addEventListener('click', () => {
+            closeModal($target);
+        });
+    });
+
+    document.addEventListener('keydown', (event) => {
+        if(event.key === "Escape") {
+            closeAllModals();
         }
     });
 }
