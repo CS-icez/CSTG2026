f6c7cbc396934e5703a15b82a27e6d5f578f12f6
icez <2067470186@qq.com>
2024-05-25 14:30:02 +0800

    redesign section page

---
+++ static/js/section.js
@@ -0,0 +1,10 @@
+$(window).on('load', function() {
+    const placeholder = $('#placeholder-right');
+    const fixed = $('#fixed-right');
+    var rect = placeholder[0].getBoundingClientRect();
+    fixed.css({
+        position: 'fixed',
+        top: rect.top + 'px',
+        left: rect.left + 'px'
+    });
+})
